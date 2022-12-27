c518eb448850aae7b16e6b1aa3583a9e30ac244d
남의영 <ny2485@naver.com>
2022-12-27 14:42:04 +0900

    init commit

---
+++ src/test/java/com/studyclub/AppTests.java
@@ -0,0 +1,13 @@
+package com.studyclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
