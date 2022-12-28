8240b203beba222794deb354789713e3466d0f42
남의영 <ny2485@naver.com>
2022-12-29 07:40:38 +0900

    [add] account Validate, mail send

--- src/main/java/com/studyclub/account/AccountController.java
+++ src/main/java/com/studyclub/account/AccountController.java
@@ -1,15 +1,66 @@
 package com.studyclub.account;
 
 
+import com.studyclub.ConsoleMailSender;
+import com.studyclub.domain.Account;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+//    @Autowired
+    private final AccountService accountService;
+    private final SignUpFormValidator signUpFormValidator;
+    private final JavaMailSender javaMailSender;
+
+//        signUpFormValidator.validate(signUpForm,errors);
+//        if (errors.hasErrors()) {
+//            return "account/sign-up";
+//        }
+    //를 initBinder로
+    //위에것이 직관적인데
+    @InitBinder("signUpForm") //SignUpForm의 변수명이아니라 타입의 카멜케이스와 매핑
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signup(Model model) {
-        return "/account/sign-up";
+
+        model.addAttribute(new SignUpForm()); //"SignUpForm"이 model로 들어감 -> Test
+        return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signupSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+        Account newAccount = accountService.saveAccount(signUpForm);
+
+        newAccount.generateEmailCheckToken();
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(newAccount.getEmail());
+        simpleMailMessage.setSubject("스터디클럽, 회원 가입 인증");
+        simpleMailMessage.setText("/check-email-token?token="+ newAccount.getEmailCheckToken()+
+                "&email=" + newAccount.getEmail());
+
+//        javaMailSender.send(simpleMailMessage);
+        ConsoleMailSender consoleMailSender = new ConsoleMailSender();
+        consoleMailSender.send(simpleMailMessage);
+        return "redirect:/";
     }
 }
