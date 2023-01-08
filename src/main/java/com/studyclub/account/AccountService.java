fbe246812078f7a5e61113c2092cb93b99fcc30e
남의영 <ny2485@naver.com>
2023-01-08 18:17:02 +0900

    Add : 닉네임으로 유저찾기 추기

--- src/main/java/com/studyclub/account/AccountService.java
+++ src/main/java/com/studyclub/account/AccountService.java
@@ -12,6 +12,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,7 +24,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class AccountService {
+public class AccountService implements UserDetailsService {
 
     private final AccountRepository accountRepository;
     private final JavaMailSender javaMailSender;
@@ -30,7 +33,7 @@ public class AccountService {
     @Transactional//
     public Account processNewAccount(SignUpForm signUpForm) {
         Account newAccount = saveAccount(signUpForm);
-        newAccount.generateEmailCheckToken(); 
+        newAccount.generateEmailCheckToken();
         sendSignUpConfirmEmail(newAccount);
         //        ConsoleMailSender consoleMailSender = new ConsoleMailSender();
         //        consoleMailSender.send(simpleMailMessage);
@@ -78,5 +81,19 @@ public class AccountService {
 //        Authentication authentication = authenticationManager.authenticate(token2);
 //        SecurityContext context2 = SecurityContextHolder.getContext();
 //        context2.setAuthentication(authentication);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String emailOrNickname) throws UsernameNotFoundException {
+        Account account = accountRepository.findByEmail(emailOrNickname);
+        if (account == null) {
+            account = accountRepository.findByNickname(emailOrNickname);
+        }
+
+        if (account == null) {
+            throw new UsernameNotFoundException(emailOrNickname);
+        }
+        return new UserAccount(account);
+
     }
 }
