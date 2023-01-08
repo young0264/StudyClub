aa2a4a122aa49142671dc5e9f73660ff65947ecc
남의영 <ny2485@naver.com>
2023-01-08 19:04:31 +0900

    Add : 로그인 성공,실패 test code

--- src/test/java/com/studyclub/main/MainControllerTest.java
+++ src/test/java/com/studyclub/main/MainControllerTest.java
@@ -1,4 +1,78 @@
+package com.studyclub.main;
+
+import com.studyclub.account.AccountService;
+import com.studyclub.account.SignUpForm;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class MainControllerTest {
-  
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    AccountService accountService;
+
+    @BeforeEach
+    void beforeEach() {
+    SignUpForm newSignUpForm = SignUpForm.builder()
+            .nickname("young")
+            .password("12341234")
+            .email("ny2485@naver.com")
+            .build();
+    accountService.processNewAccount(newSignUpForm);
+    }
+    @Test
+    @DisplayName("이메일로 로그인 ")
+    void login_with_email() throws Exception {
+
+        mockMvc.perform(post("/login")
+                        .param("username", "ny2485@naver.com") //기본 파라미터
+                        .param("password", "12341234")
+                        .with(csrf())
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(authenticated().withUsername("young"));
+    }
+
+    @Test
+    @DisplayName("아이디로 로그인 ")
+    void login_with_nickname() throws Exception {
+        mockMvc.perform(post("/login")
+                        .param("username", "young") //기본 파라미터
+                        .param("password", "12341234")
+                        .with(csrf())
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(authenticated().withUsername("young"));
+    }
+
+    @Test
+    @DisplayName("로그인 실패")
+    void login_fail() throws Exception {
+        mockMvc.perform(post("/login")
+                        .param("username", "1212") //기본 파라미터
+                        .param("password", "1212")
+                        .with(csrf())
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?error"))
+                .andExpect(unauthenticated());
+    }
 }
