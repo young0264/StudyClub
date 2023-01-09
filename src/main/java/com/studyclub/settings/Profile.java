dc6928239cbb77fa1e1276c969e5cdb5407613a9
남의영 <ny2485@naver.com>
2023-01-09 23:57:17 +0900

    Add : Profile domain추가

--- src/main/java/com/studyclub/settings/Profile.java
+++ src/main/java/com/studyclub/settings/Profile.java
@@ -1,2 +1,18 @@
-package com.studyclub.settings;public class Profile {
+package com.studyclub.settings;
+
+import com.studyclub.domain.Account;
+
+public class Profile {
+
+    private String bio; //프로필
+
+    private String url;
+
+    private String occupation; //직업
+
+    private String location; //살고있는 지역 //varchar(255)
+
+    public Profile(Account account) {
+        this.bio = account.getBio();
+    }
 }
