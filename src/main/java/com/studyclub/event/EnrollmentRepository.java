598505b301522ddbb5821190f633bf19e3273de1
남의영 <ny2485@naver.com>
2023-01-25 23:13:12 +0900

    Add: Event 모임 추가 수정 삭제

--- src/main/java/com/studyclub/event/EnrollmentRepository.java
+++ src/main/java/com/studyclub/event/EnrollmentRepository.java
@@ -1,2 +1,12 @@
-package com.studyclub.event;public interface EnrollmentRepository {
+package com.studyclub.event;
+
+import com.studyclub.domain.Account;
+import com.studyclub.domain.Enrollment;
+import com.studyclub.domain.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+    boolean existsByEventAndAccount(Event event, Account account);
+
+    Enrollment findByEventAndAccount(Event event, Account account);
 }
--- src/main/java/com/studyclub/event/EventRepository.java
+++ src/main/java/com/studyclub/event/EventRepository.java
@@ -1,2 +1,16 @@
-package com.studyclub.event;public class EventRepository {
+package com.studyclub.event;
+
+import com.studyclub.domain.Event;
+import com.studyclub.domain.Study;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional(readOnly = true)
+public interface EventRepository  extends JpaRepository<Event, Long> {
+//    List<Event> findByStudyOrderByStartDateTime(Study study);
+    @EntityGraph(value = "Event.withEnrollments", type = EntityGraph.EntityGraphType.LOAD)
+    List<Event> findByStudyOrderByStartDateTime(Study study);
 }
