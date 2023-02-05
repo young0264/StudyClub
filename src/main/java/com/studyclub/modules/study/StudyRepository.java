3fcdd6ded6e78914cc59fc208551aae2a0d6949e
남의영 <ny2485@naver.com>
2023-02-05 11:31:14 +0900

    Refactor : notification for study update

--- src/main/java/com/studyclub/modules/study/StudyRepository.java
+++ src/main/java/com/studyclub/modules/study/StudyRepository.java
@@ -10,24 +10,22 @@ import org.springframework.transaction.annotation.Transactional;
 public interface StudyRepository extends JpaRepository<Study, Long> {
     boolean existsByPath(String path);
 
-    @EntityGraph(value = "Study.withAll", type=EntityGraph.EntityGraphType.LOAD)
+    @EntityGraph(attributePaths = {"tags", "managers", "zones", "members"})
     Study findByPath(String path);
-    @EntityGraph(value = "Study.withTagsAndManagers", type = EntityGraph.EntityGraphType.FETCH)
+    @EntityGraph(attributePaths = {"tags","managers"})
     Study findStudyWithTagsByPath(String path);
 
-    @EntityGraph(value = "Study.withZonesAndManagers", type = EntityGraph.EntityGraphType.FETCH)
+    @EntityGraph(attributePaths = {"zones","managers"})
     Study findStudyWithZonesByPath(String path);
 
-    @EntityGraph(value = "Study.withManagers", type = EntityGraph.EntityGraphType.FETCH)
+    @EntityGraph(attributePaths = {"managers"})
     Study findStudyWithManagersByPath(String path);
 
-    @EntityGraph(value = "Study.withMembers", type = EntityGraph.EntityGraphType.FETCH)
+    @EntityGraph(attributePaths = {"members"}) // FETCH가 기본
     Study findStudyWithMembersByPath(String path);
-
 
     Study findStudyOnlyByPath(String path);
 
-    //    @EntityGraph(value = "Study.withTagsAndZones", type = EntityGraph.EntityGraphType.FETCH)
     @EntityGraph(attributePaths = {"zones", "tags"})
     Study findStudyWithTagsAndZonesById(Long id);
 
--- src/main/java/com/studyclub/modules/study/event/StudyUpdateEvent.java
+++ src/main/java/com/studyclub/modules/study/event/StudyUpdateEvent.java
@@ -1,2 +1,16 @@
-package com.studyclub.modules.study.event;public class StudyUpdateEvent {
+package com.studyclub.modules.study.event;
+
+import com.studyclub.modules.study.Study;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class StudyUpdateEvent {
+
+    private final Study study;
+    private final String message;
 }
+
+
+
